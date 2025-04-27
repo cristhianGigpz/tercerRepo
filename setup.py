b31a7d3b23cf468475662ba85467a864ca7ee6cd
cristhianGigpz <cjacevedot@gmail.com>
2025-04-27 13:41:27 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Cristhian Acevedo",                         # Tu nombre
+    author_email="cjacevedot@gigpz.com",                 # Tu correo electrónico
+    url="https://github.com/cristhianGigpz/tercerRepo",     # URL del proyecto
+)
